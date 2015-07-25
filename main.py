179331a70986f0bd3ade92ec760d1540eaa4de1d
Rolando Urquiza <r.urquiza@lab.matcom.uh.cu>
2015-07-25 16:30:33 -0400

    Added program entry point.

---
+++ main.py
@@ -0,0 +1,14 @@
+from PyQt5.QtCore import QUrl
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtQml import QQmlApplicationEngine
+
+import resources_qrc
+
+if __name__ == '__main__':
+    import sys
+    app = QApplication(sys.argv)
+
+    engine = QQmlApplicationEngine()
+    engine.load(QUrl('qrc:/qml/main.qml'))
+
+    sys.exit(app.exec_())
